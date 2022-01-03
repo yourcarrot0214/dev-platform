8a681053f5870c3b3147d73562772cc3d0dd0857
yourcarrot0214 <stylenbs@gmail.com>
2022-01-03 22:50:23 +0900

    feature : MongoDB Access

---
+++ utils/mongodb/mongodb.ts
@@ -0,0 +1,24 @@
+import { MongoClient } from "mongodb";
+
+const MONGO_URI = process.env.MONGODB_URI as string;
+const options = {};
+
+let client;
+let clientPromise;
+
+if (!process.env.MONGODB_URI) {
+  throw new Error("Please add your Mongo URI to .env.local");
+}
+
+if (process.env.NODE_ENV === "development") {
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(MONGO_URI, options);
+    global._mongoClientPromise = client.connect();
+  }
+  clientPromise = global._mongoClientPromise;
+} else {
+  client = new MongoClient(MONGO_URI, options);
+  clientPromise = client.connect();
+}
+
+export default clientPromise;
