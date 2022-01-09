be1b8f940171869180173db93340363dc9474bfc
yourcarrot0214 <stylenbs@gmail.com>
2022-01-09 21:17:49 +0900

    fix/FE : signup response user data에서 password정보가 삭제되지 않는 문제, login시 유저의 비밀번호와 암호화된 비밀번호가 일치하지 않는 문제

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -1,24 +1,53 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { connect } from "../../../utils/mongodb/mongodb";
 import { ResponseFuncs } from "../../../types/config";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { StoredUserType } from "../../../types/user";
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const method: keyof ResponseFuncs = req.method as keyof ResponseFuncs;
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === "POST") {
+    const { email, password } = req.body;
+    console.log({ email, password });
+    if (!email || !password) {
+      res.statusCode = 400;
+      return res.send("필수 정보가 없습니다.");
+    }
 
-  const catcher = (error: Error) => res.status(400).json({ error });
+    const { User } = await connect();
+    const user: StoredUserType = await User.findOne({
+      email: email,
+    });
+    console.log("user : ", user);
 
-  const handleCase: ResponseFuncs = {
-    // ? RESPONSE FOR GET REQUEST
-    GET: async (req: NextApiRequest, res: NextApiResponse) => {
-      const { User } = await connect();
-      res.json(await User.find({}).catch(catcher));
-      res.end();
-    },
-  };
+    if (!user) {
+      res.statusCode = 404;
+      return res.send("해당 이메일에 해당하는 유저가 없습니다.");
+    }
 
-  const response = handleCase[method];
-  if (response) response(req, res);
-  else res.status(400).json({ error: "No Response for This Request" });
+    // ! 비밀번호 불일치
+    const isPasswordMatched = bcrypt.compareSync(password, user.password);
+    console.log("isPasswordMatched : ", isPasswordMatched);
+    if (!isPasswordMatched) {
+      res.statusCode = 403;
+      return res.send("비밀번호가 일치하지 않습니다.");
+    }
+
+    const token = jwt.sign(String(user._id), process.env.JWT_SECRET!);
+    res.setHeader(
+      "Set-cookie",
+      `access_token=${token}; path=/; expires=${new Date(
+        Date.now() + 60 * 60 * 1000 * 24
+      ).toUTCString()}; httponly`
+    );
+
+    const userWithoutPassword: Partial<Pick<StoredUserType, "password">> = user;
+    delete userWithoutPassword.password;
+
+    res.statusCode = 200;
+    return res.send(userWithoutPassword);
+  }
+
+  res.statusCode = 405;
+  return res.end();
 };
-
-export default handler;
--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -6,63 +6,6 @@ import jwt from "jsonwebtoken";
 import { v4 as uuidv4 } from "uuid";
 import { StoredUserType } from "../../../types/user.d";
 
-// const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-//   const method: keyof ResponseFuncs = req.method as keyof ResponseFuncs;
-
-//   const catcher = (error: Error) => res.status(400).json({ error });
-
-//   const handleCase: ResponseFuncs = {
-//     // ? RESPONSE FOR POST REQUEST
-//     POST: async (req: NextApiRequest, res: NextApiResponse) => {
-//       console.log("POST RESPONSE");
-//       try {
-//         // * 1. request data 검증
-//         const { email, password, name } = req.body;
-//         console.log(req.body);
-//         if (!email || !password || !name) {
-//           res.statusCode = 400;
-//           console.log("회원가입에 필요한 필수 데이터가 없습니다.");
-//           return res.send("회원가입에 필요한 필수 데이터가 없습니다.");
-//         }
-
-//         // * 2. 이미 가입된 회원 여부 검증
-//         const { User } = await connect();
-//         const userExist = await User.findOne({ email: email });
-
-//         if (userExist) {
-//           res.statusCode = 409;
-//           res.send("이미 가입된 이메일 주소 입니다.");
-//         }
-
-//         // * 3. 비밀번호 암호화 및 유저 정보 생성
-//         const hashedPassword = bcrypt.hashSync(password, 8);
-//         const userId = uuidv4();
-
-//         const newUser: StoredUserType = {
-//           _id: userId,
-//           email,
-//           name,
-//           password: hashedPassword,
-//           profileImage: "/static/image/user/default_user_profile_image.jpeg",
-//         };
-
-//         return res.status(200).json(await User.create(newUser).catch(catcher));
-//       } catch (error) {
-//         console.log("signup error : ", error.message);
-//         res.statusCode = 500;
-//         return res.send(error);
-//       }
-//     },
-//   };
-
-//   const response = handleCase[method];
-//   console.log("response : ", response);
-//   if (response) response(req, res);
-//   else res.status(400).json({ error: "No Response for This Request" });
-// };
-
-// export default handler;
-
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
     // * 1. request data 검증
@@ -113,6 +56,10 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       "password"
     >> = userData;
     delete userDataWithoutPassword.password;
+    console.log("userDataWithoutPassword : ", userDataWithoutPassword);
+    console.log("userData : ", userData);
+
+    // ! password 정보가 삭제되지 않음.
 
     res.statusCode = 200;
     return res.send(userDataWithoutPassword);
