0c41272741d0b294e6451918cfe1fe140b39c530
yourcarrot0214 <stylenbs@gmail.com>
2022-07-10 22:49:12 +0900

    update/FE - 새 글 작성 및 비로그인 유저 경고 메시지 출력 기능 구현

--- components/views/board/Board.tsx
+++ components/views/board/Board.tsx
@@ -1,10 +1,8 @@
-import React, { useEffect } from "react";
+import Link from "next/link";
 import Styled from "styled-components";
 import { useSelector } from "../../../store";
-import { useRouter } from "next/router";
 
 import PostCard from "./common/main/PostCard";
-import { CommentType, RepliesType } from "../../../types/post";
 
 const Container = Styled.div`
   width: 100%;
@@ -13,6 +11,30 @@ const Container = Styled.div`
   padding: 1rem 0;
 
   display: flex;
+  flex-direction: column;
+
+  .post-menu-bar {
+    padding: 1rem;
+    .new-posting {
+      background: none;
+      color: #161616;
+      outline: none;
+      border: 1px solid #161616;
+      border-radius: 1rem;
+      padding: 0.25rem 0.5rem;
+      transition: all 0.125s ease-in 0s;
+
+      :hover {
+        background: #161616;
+        color: white;
+        border: 1px solid white;
+      }
+    }
+    .warning {
+      font-size: 1rem;
+      color: #d50000;
+    }
+  }
 
   .post-list-wrapper {
     flex: 1 1 0%;
@@ -28,6 +50,7 @@ const Board: React.FC = () => {
   const { postlist, commentlist, replieslist } = useSelector(
     (state) => state.board
   );
+  const isLogged = useSelector((state) => state.user.isLogged);
 
   function postDataList() {
     let result = [];
@@ -48,6 +71,17 @@ const Board: React.FC = () => {
 
   return (
     <Container>
+      <div className="post-menu-bar">
+        {isLogged ? (
+          <Link href="/board/write">
+            <button className="new-posting">새 글 작성</button>
+          </Link>
+        ) : (
+          <h3 className="warning">
+            📣 새 글 작성은 로그인 유저에게만 제공됩니다.
+          </h3>
+        )}
+      </div>
       <div className="post-list-wrapper">
         <div className="post-list">
           {postDataList().map((postData) => (
@@ -79,4 +113,8 @@ export default Board;
         - user profile image
         - user name
         - replies count
+
+  TODO : 새글 작성 버튼 및 로그인 안내
+    ? 로그인된 유저에게는 새글 작성 버튼을 출력하고 비로그인 유저에게는 안내 메시지를 출력하기
+    ? .post-list 상단에 출력되며 state.user.isLogged 값에 따라 다른 컴포넌트를 출력
 */
